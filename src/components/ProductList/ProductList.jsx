6df592a74739a1c1f36c49077ba8681614e1f10c
VicWrest <cyganov0206@gmail.com>
2024-10-18 23:12:13 +1000

    init2

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://80.93.191.174:8000/web-data', {
+        fetch('http://80.93.191.174/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
